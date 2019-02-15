a00f719facdeefe85b85d4828b620a0354212785
Nils Hörrmann <nils.hoerrmann@hananils.de>
2019-02-15 12:04:20 +0100

    Update files and users fields

--- src/Converters/Fields/Files.php
+++ src/Converters/Fields/Files.php
@@ -2,11 +2,22 @@
 
 namespace Hananils\Converters\Fields;
 
+use Hananils\Converters\File;
 use Hananils\Xml;
 
 class Files extends Xml
 {
     public function parse($field, $blueprint)
     {
+        if ($field->isEmpty()) {
+            return;
+        }
+
+        foreach ($field->toFiles() as $child) {
+            $user = new File('file');
+            $user->import($child);
+
+            $this->addElement('file', $user->root());
+        }
     }
 }
--- src/Converters/Fields/Users.php
+++ src/Converters/Fields/Users.php
@@ -2,6 +2,7 @@
 
 namespace Hananils\Converters\Fields;
 
+use Hananils\Converters\User;
 use Hananils\Xml;
 
 class Users extends Xml
@@ -12,12 +13,11 @@ class Users extends Xml
             return;
         }
 
-        foreach ($field->toPages() as $child) {
+        foreach ($field->toUsers() as $child) {
             $user = new User('user');
             $user->import($child);
 
             $this->addElement('user', $user->root());
         }
-
     }
 }
