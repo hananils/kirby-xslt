f484656e7281b689df86efadba59086cf2b9c4ea
Nils Hörrmann <nils.hoerrmann@hananils.de>
2022-08-30 09:08:32 +0200

    Update image informations

--- src/Converters/File.php
+++ src/Converters/File.php
@@ -57,52 +57,73 @@ class File extends Xml
 
             if (isset($options['crop']) && strpos($options['crop'], '.') > 0) {
                 $positions = [
-                    "top left", "top", "top right", "left", "center", "right", "bottom left", "bottom", "bottom right"
+                    'top left',
+                    'top',
+                    'top right',
+                    'left',
+                    'center',
+                    'right',
+                    'bottom left',
+                    'bottom',
+                    'bottom right'
                 ];
 
                 $name = explode('.', $options['crop'])[1];
                 $field = $file->content()->get($name);
                 $type = $file->blueprint()->field($name)['type'];
 
-                if ($type !== 'focus' && in_array($field->value(), $positions)) {
+                if (
+                    $type !== 'focus' &&
+                    in_array($field->value(), $positions)
+                ) {
                     $options['crop'] = $field->value();
                 } else {
                     $options['crop'] = 'center';
                 }
             }
 
+            $width = isset($options['width']) ? $options['width'] : null;
+            $height = isset($options['height']) ? $options['height'] : null;
             if ($type === 'focus') {
-                $width = isset($options['width']) ? $options['width'] : null;
-                $height = isset($options['height']) ? $options['height'] : null;
                 $thumb = $file->focusCrop($width, $height, $options);
             } else {
                 $thumb = $file->thumb($options);
             }
 
+            // Set image attributes
             $attributes = [];
             foreach ($thumb->modifications() as $key => $value) {
                 $key = Str::kebab($key);
                 if (is_bool($value)) {
-                    $attributes[$key] = ($value === true ? 'true' : 'false');
+                    $attributes[$key] = $value === true ? 'true' : 'false';
                 } else {
                     $attributes[$key] = $value;
                 }
             }
 
-            if(!isset($attributes['height']) && isset($width)) {
-                $attributes['height'] = $width / $file->width() * $file->height();
-            } else {
-                $attributes['height'] = $file->height();
+            // Set height
+            if (empty($attributes['height']) && $width) {
+                $attributes['height'] = round(
+                    ($width / $file->width()) * $file->height()
+                );
             }
 
-            if(!isset($attributes['width']) && isset($height)) {
-                $attributes['width'] = $height / $file->height() * $file->width();
-            } else {
-                $attributes['width'] = $file->width();
+            // Set width
+            $attributes['original-width'] = $file->width();
+            if (!isset($attributes['width']) && $height) {
+                $attributes['width'] = round(
+                    ($height / $file->height()) * $file->width()
+                );
             }
+
+            // Set original dimensions
+            $attributes['original-width'] = $file->width();
+            $attributes['original-height'] = $file->height();
+
+            // Add cache status
+            $attributes['cache'] = $thumb->exists() ? 'true' : 'false';
 
             $this->addElement('url', $thumb->url(), $attributes, $thumbs);
         }
     }
-
 }
